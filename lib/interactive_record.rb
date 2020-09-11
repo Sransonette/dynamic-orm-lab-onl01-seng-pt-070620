fbe4bdc2f9a68bb937b67eb5419f2ab3a180e719
Sean Ransonette <sransonette@gmail.com>
2020-09-11 23:41:30 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -58,10 +58,10 @@ class InteractiveRecord
   end
   
   def self.find_by(name)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
     
     
-    
-    DB[:conn].execute(name)
+    DB[:conn].execute( name)
   end 
   
   
